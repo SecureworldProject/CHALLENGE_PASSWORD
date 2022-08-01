d09cc8def86e888d4c849e42e80ba7b123460d82
jose javier garcia aranda <jjaranda13@hotmail.com>
2022-08-01 13:25:33 +0200

    chpass v1

---
+++ chpass.py
@@ -0,0 +1,80 @@
+from math import log10, sqrt
+
+import os
+from pathlib import Path
+import time
+#para instalar el modulo easygui simplemente:
+#pip3 install easygui
+# o bien py -m pip install easygui
+import easygui 
+
+# variables globales
+# ------------------
+props_dict={} 
+DEBUG_MODE=True
+
+def init(props):
+    global props_dict
+    print("Python: Enter in init")
+    
+    #props es un diccionario
+    props_dict= props
+    res=executeChallenge()
+    #check len retornada 
+    if (res[1]>0):
+        return 0
+    else:
+        #no se ha podido ejecutar
+        return -1
+
+
+
+def executeChallenge():
+    print("Starting execute")
+    
+    
+    #mecanismo de lock BEGIN, para garantizar una sola interaccion con user a la vez
+    #-----------------------
+    folder=os.environ['SECUREMIRROR_CAPTURES']
+    while os.path.exists(folder+"/"+"lock"):
+        time.sleep(1)
+    Path(folder+"/"+"lock").touch()
+
+    #pedimos password
+    clave = easygui.enterbox("enter password", "chpass", "")
+
+    #mecanismo lock
+    os.remove(folder+"/"+"lock")
+    
+    #ahora comparamos con la correcta
+    correcta=props_dict["param1"]
+
+    #construccion de la respuesta
+    mode= props_dict["mode"]
+
+    if (mode=="parental"):
+        if (clave==correcta):
+            cad="%d"%(1)
+        else:
+            cad="%d"%(0)
+    else:#modo no parental
+        cad=clave
+    
+    key = bytes(cad,'utf-8')
+    key_size = len(key)
+
+    result =(key, key_size)
+    print ("result:",result)
+    return result
+
+
+
+    
+
+
+if __name__ == "__main__":
+    #mode "parental" o "normal"
+    midict={"param1": "clavesecreta", "mode":"parental"}
+    init(midict)
+    #executeChallenge()
+
